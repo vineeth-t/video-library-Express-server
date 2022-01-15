f798671e99ce4aa19d148dfe4dcf7963877187d8
Pavan Vineethreddy Thaviti <74752572+vineeth-t@users.noreply.github.com>
2022-01-15 07:39:53 +0000

    Edit password through front end

--- Routes/users.route.js
+++ Routes/users.route.js
@@ -1,12 +1,38 @@
-const express=require('express')
-const router=express.Router();
-const { UserModel } = require('../Models/user.model')
+const express = require('express')
+const router = express.Router();
+const { UserModel } = require('../Models/user.model');
+const bodyParser=require('body-parser')
+
+router.use(bodyParser.json())
+
+const bcrypt = require('bcryptjs');
+const extend = require('lodash')
 
 router.route('/')
 
-.get(async(req,res)=>{
-  const userId=req.userId;
-  const {firstname,lastname,username}=await UserModel.findById(userId)
-  res.json({response:true,firstname,lastname,username})
-})
-module.exports=router
+  .get(async (req, res) => {
+    const userId = req.userId;
+    const { firstname, lastname, username } = await UserModel.findById(userId)
+    res.json({ response: true, firstname, lastname, username })
+  })
+
+  .post(async (req, res) => {
+    const userId = req.userId;
+    console.log(req.body)
+    const { currentPassword, newPassword } = req.body
+    
+    const findUser = await UserModel.findById(userId);
+    if (await bcrypt.compare(currentPassword, findUser.password)) {
+      findUser.password = newPassword
+      const salts = 10
+      findUser.password = await bcrypt.hash(findUser.password, salts)
+       user = await findUser.save();
+       res.json({ response: true,message:'password changed Successfully'})
+    }else{
+      res.json({ response: true,message:'In-Correct Current password'})
+    }
+
+
+
+  })
+module.exports = router
